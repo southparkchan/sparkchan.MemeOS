a083d23333af09de355dfb71e298c2f4e432fa9f
sparkchan <zaenxbt@gmail.com>
2025-11-18 03:57:34 +0700

    Update layout.js

--- apps/playground/src/app/layout.js
+++ apps/playground/src/app/layout.js
@@ -1,13 +1,12 @@
-import Providers from "./providers";
+export const metadata = {
+  title: "MemeOS",
+  description: "Next generation Solana meme operating system"
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>
-        <Providers>
-          {children}
-        </Providers>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
