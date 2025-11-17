e3b2cd03f610e9e268a34bde98d9d882e70d3b46
sparkchan <zaenxbt@gmail.com>
2025-11-18 00:50:42 +0700

    Create page.js

---
+++ apps/playground/src/app/page.js
@@ -0,0 +1,17 @@
+import WalletButton from "@/components/WalletButton";
+
+export default function Home() {
+  return (
+    <div className="p-8 flex flex-col gap-6">
+      <h1 className="text-4xl font-bold text-white">MemeOS Playground</h1>
+
+      <WalletButton />
+
+      <div className="bg-neutral-900 p-6 rounded-xl border border-neutral-700">
+        <p className="text-neutral-300">
+          Kamu sudah bisa connect/disconnect wallet + siap dipublish.
+        </p>
+      </div>
+    </div>
+  );
+}
