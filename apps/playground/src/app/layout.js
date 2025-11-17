6476afb55bdb4c6272f4f345628080f88315e653
sparkchan <zaenxbt@gmail.com>
2025-11-18 00:50:09 +0700

    Create layout.js

---
+++ apps/playground/src/app/layout.js
@@ -0,0 +1,13 @@
+import Providers from "./providers";
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
